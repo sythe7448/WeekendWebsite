ad45cc429ac2786fb0d85b207e0fd25f8d5aa7cf
Jake Colburn <jakecolburn.sythe@gmail.com>
2017-03-10 17:40:47 -0500

    Added functions that produce all 6 stats for character creation. rollstatspretty() function makes and explains how a stat is generated.

--- Laravel/public/index.php
+++ Laravel/public/index.php
@@ -1,27 +1,55 @@
 <html>
 <head>
-    <title>hello</title>
+    <title>Stat Roller</title>
 </head>
 <body>
 <?php
-function rollstats()
+function rollstatspretty()
 {
-    //D6 rolling for character stats.
+    //This Function just explains how a stat is generated when ran. Old code probably going to remove.
     echo "Rolling 3d6 drop the lowest, <br />\n";
     $rollcount = 0;
-    do {
+    while ($rollcount < 4) {
         $roll = rand(1, 6);
         $rollcount++;
         //echo $roll . ",";
         $result[$rollcount] = $roll;
-    } while ($rollcount < 4);
+    }
     sort($result);
     echo "you rolled " . join(', ', $result) . " take off the " . $result[0];
     array_shift($result);
     //echo join(", ", $result);
     echo "<br />\n and your Score is: " . array_sum($result) . "<br />\n";
 }
-rollstats();
+//rollstatspretty();
+?>
+<?php
+//This Function generates a single stat for characters or NPCs.
+function rollstat()
+{
+    //D6 rolling for character stats.
+    $rollcount = 0;
+    while ($rollcount < 4) {
+        $roll = rand(1, 6);
+        $rollcount++;
+        $result[$rollcount] = $roll;
+    }
+    sort($result);
+    array_shift($result);
+    //return array_sum($result);
+    return array_sum($result);
+}
+//This Function reruns the above function 6 times to generate all the primary stats.
+function stats() {
+    $statrolls = 0;
+    while ($statrolls < 6) {
+        $stat = rollstat();
+        $statrolls++;
+        $rolledstats[$statrolls] = $stat;
+    }
+    print join(",", $rolledstats);
+}
+stats();
 ?>
 </body>
 </html>
