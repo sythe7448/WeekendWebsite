f12f279a20259ff791399b013d0412e8d289c0ca
Jake Colburn <jakecolburn.sythe@gmail.com>
2017-03-10 16:20:44 -0500

    D6 roll function

--- Laravel/public/index.php
+++ Laravel/public/index.php
@@ -4,20 +4,24 @@
 </head>
 <body>
 <?php
-    //D6 rolling
-    echo "Rolling 3d6 drop the lowest, \n";
+function rollstats()
+{
+    //D6 rolling for character stats.
+    echo "Rolling 3d6 drop the lowest, <br />\n";
     $rollcount = 0;
     do {
-        $roll = rand (1,6);
-        $rollcount ++;
+        $roll = rand(1, 6);
+        $rollcount++;
         //echo $roll . ",";
         $result[$rollcount] = $roll;
-    }while($rollcount < 4);
+    } while ($rollcount < 4);
     sort($result);
     echo "you rolled " . join(', ', $result) . " take off the " . $result[0];
     array_shift($result);
     //echo join(", ", $result);
-    echo " and your Score is: " . array_sum($result) . "\n";
+    echo "<br />\n and your Score is: " . array_sum($result) . "<br />\n";
+}
+rollstats();
 ?>
 </body>
 </html>
